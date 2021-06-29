6b86a5526811d63d4afc995c430e2012fc9efbdd
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-29 16:30:03 -0700

    signup via post test created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,22 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
+describe('User routes', () => {
+  beforeAll(() => {
     return setup(pool);
+  });
+
+  it('signup user with POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'this is an email',
+        password: 'password'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'this is an email',
+    });
   });
 });
