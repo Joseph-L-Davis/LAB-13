626b145456b129ece0c7b534c261527e2e7f62f8
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-29 21:46:44 -0700

    test failing...user.authToken not a func..

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,5 +3,5 @@ CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    profilePhotoUrl TEXT NOT NULL
+    profile_photo_url TEXT NOT NULL
 );
